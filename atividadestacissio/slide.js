8b7cf9b4736ec381e1ab249d2b1da1df390c79b7
cleniospider <clenio.lopes@aluno.ifsertao-pe.edu.br>
2026-06-19 10:34:56 -0300

    Create slide.js

---
+++ atividadestacissio/slide.js
@@ -0,0 +1,32 @@
+let nomeUsuario = "Aphonsa"; 
+nomeUsuario = "Aphonsa Nunes"; 
+
+const anoAtual = 2026;
+
+let texto = "Olá, Mundo!";        // String (Texto)
+let numeroInteiro = 25;           // Number (Número inteiro)
+let numeroDecimal = 99.90;        // Number (Número com ponto flutuante)
+let estaLogado = true;            // Boolean (Verdadeiro ou Falso)
+let valorNulo = null;             // Null (Vazio intencional)
+let indefinido;                   // Undefined (Declarada, mas sem valor)
+
+
+console.log("--- Demonstração das Variáveis ---");
+console.log("Nome do Usuário:", nomeUsuario);
+console.log("Ano Atual (Constante):", anoAtual);
+console.log("Tipo String:", texto);
+console.log("Tipo Number:", numeroInteiro, "e", numeroDecimal);
+console.log("Tipo Boolean:", estaLogado);
+console.log("Tipo Null:", valorNulo);
+console.log("Tipo Undefined:", indefinido);
+
+
+
+
+
+
+
+
+
+
+
